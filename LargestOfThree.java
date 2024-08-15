449af148e156be89c872eb146208cf82550357b5
Jonathonu373 <uribejonathon@gmail.com>
2024-08-15 12:12:00 -0700

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,58 @@
+//This is a comment
+/*
+ *Jonathon Uribe
+ * 08/15/2024
+ * AddTwoints.java
+ */
+
+import java.util.Scanner;  // Import the Scanner class
+
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        System.out.println("\n\n Welcome is the Double Int program ! \n\n");
+
+        int firstNum = 0;
+        int secondNum = 0;
+        int thirdNum = 0;
+        int largestOfThree = 0;
+
+
+        //Create a scanner object
+        Scanner myScannerObject = new Scanner(System.in);
+
+        // Ask user for the first int
+        System.out.println("\n Please enter a whole number: ");
+        firstNum = myScannerObject.nextInt();
+
+
+        // Ask user for the second int.
+        System.out.println("\n Please enter a second whole number: ");
+        secondNum = myScannerObject.nextInt();
+
+        // Ask user for the second int.
+        System.out.println("\n Please enter a third whole number: ");
+        thirdNum = myScannerObject.nextInt();
+
+
+        // Processing:
+        // Compare the inputs with nested if statements.
+
+        if (firstNum>secondNum)
+        {
+            if (firstNum > thirdNum)
+            {
+                largestOfThree = firstNum;
+        else
+        {
+            largestOfThree = thirdNum
+        }
+        }
+        else{
+
+        if (secondNum > thirdNum)
+        {else
+
+
+            //Output largest of three
+        System.out.println("\n The largest of three is: : + largestOfThree");

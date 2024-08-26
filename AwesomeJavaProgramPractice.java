2253529032690b17c5ce654825ccbcacd3a254c9
Jonathonu373 <uribejonathon@gmail.com>
2024-08-25 21:52:27 -0700

    Add files via upload

---
+++ AwesomeJavaProgramPractice.java
@@ -0,0 +1,18 @@
+//Practice programs
+//My name and shoe size practice.
+
+public class AwesomeJavaProgramPractice {
+    public static void main(String[] args) {
+        int myInt =7;
+        double shoeSize =9.5;
+        char myInitial = 'J';
+
+
+        double result = myInt * shoeSize;
+        System.out.println(myInt * shoeSize);
+
+    }
+    String myName = "John"; {
+        System.out.println(myName.length());}
+
+}
